64214a9ffbc58f702fed39021df05f6e7e3209b5
Hariharaselvi <hariharaselvi1997@gmail.com>
2026-01-19 14:21:45 +0300

    Fix booking suspense issue

--- app/booking/page.tsx
+++ app/booking/page.tsx
@@ -1,10 +1,14 @@
 'use client'; 
 export const dynamic = "force-dynamic";
 import BookingComponent from "./BookingComponent";
-
+import { Suspense } from "react";
 
 
 
 export default function Booking() {
-  return <BookingComponent />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <BookingComponent />
+    </Suspense>
+  )
 }
